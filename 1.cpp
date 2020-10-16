54cf21f337b85ba27f23fcb7482142189a1c61d0
sbyoon0 <72955402+sbyoon0@users.noreply.github.com>
2020-10-16 11:19:17 +0900

    Revert "1.cpp"

--- 1.cpp
+++ 1.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 void main() {
-	for (int i = 1; i < 10; i++) { // 9 -> 10 으로 수정부탁드립니다. 
+	for (int i = 1; i < 9; i++) { // 9 -> 10 으로 수정부탁드립니다. 
 		for (int j = 1; j < 10; i++) {
 			cout << i * j << endl;
 		}
 	}
 }
-
