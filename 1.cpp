9ed9d5b745b6b975547c8a350e33283156e8ea72
grail <sbyoon0228@gmail.com>
2020-10-16 11:02:31 +0900

    Non-modified

---
+++ 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+void main() {
+	for (int i = 1; i < 9; i++) { // 9 -> 10 으로 수정부탁드립니다. 
+		for (int j = 1; j < 10; i++) {
+			cout << i * j << endl;
+		}
+	}
+}
